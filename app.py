8f931dc76869a35f48da165385f97b507e37a482
Rininmmmment <101512834+Rininmmmment@users.noreply.github.com>
2022-11-09 23:51:26 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -23,108 +23,102 @@ st.subheader("Input Data")
 st.dataframe(data, width=500, height=200)
 
 #G初期値
-try:
-    plt.scatter(ch, element, s=1)
-    plt.xlabel('CH')
-    plt.ylabel('count')
+plt.scatter(ch, element, s=1)
+plt.xlabel('CH')
+plt.ylabel('count')
 
-    st.markdown("---")
-    st.subheader("Initial Value")
+st.markdown("---")
+st.subheader("Initial Value")
 
-    shoki = ch.min() + (ch.max() - ch.min()) / 2
-    count = st.sidebar.number_input("Number of Functions", 1, 4, 1)
+shoki = ch.min() + (ch.max() - ch.min()) / 2
+count = st.sidebar.number_input("Number of Functions", 1, 4, 1)
+st.sidebar.markdown("---")
+amp1 = st.sidebar.slider("A(green)", 0, element.max(), int(element.max()/2))
+ctr1 = st.sidebar.slider("μ(green)", 0, ch.max(), int(shoki))
+wid1 = st.sidebar.slider("width(green)", 0, 30, int(shoki/10))
+g1 = amp1 * np.exp( -((ch - ctr1)/wid1)**2)
+plt.fill_between(ch, g1, 0, facecolor='lime', alpha=0.3)
+y = g1
+
+if count > 1:
     st.sidebar.markdown("---")
-    amp1 = st.sidebar.slider("A(green)", 0, element.max(), int(element.max()/2))
-    ctr1 = st.sidebar.slider("μ(green)", 0, ch.max(), int(shoki))
-    wid1 = st.sidebar.slider("width(green)", 0, 30, int(shoki/10))
-    g1 = amp1 * np.exp( -((ch - ctr1)/wid1)**2)
-    plt.fill_between(ch, g1, 0, facecolor='lime', alpha=0.3)
-    y = g1
+    amp2 = st.sidebar.slider("A(blue)", 0, element.max(), int(element.max()/2))
+    ctr2 = st.sidebar.slider("μ(blue)", 0, ch.max(), int(shoki)+5)
+    wid2 = st.sidebar.slider("width(blue)", 0, 30, int(shoki/10))
+    g2 = amp2 * np.exp( -((ch - ctr2)/wid2)**2)
+    plt.fill_between(ch, g2, 0, facecolor='blue', alpha=0.3)
+    y = g1 + g2
 
-    if count > 1:
-        st.sidebar.markdown("---")
-        amp2 = st.sidebar.slider("A(blue)", 0, element.max(), int(element.max()/2))
-        ctr2 = st.sidebar.slider("μ(blue)", 0, ch.max(), int(shoki)+5)
-        wid2 = st.sidebar.slider("width(blue)", 0, 30, int(shoki/10))
-        g2 = amp2 * np.exp( -((ch - ctr2)/wid2)**2)
-        plt.fill_between(ch, g2, 0, facecolor='blue', alpha=0.3)
-        y = g1 + g2
+if count > 2:
+    st.sidebar.markdown("---")
+    amp3 = st.sidebar.slider("A(pink)", 0, element.max(), int(element.max()/2))
+    ctr3 = st.sidebar.slider("μ(pink)", 0, ch.max(), int(shoki)+10)
+    wid3 = st.sidebar.slider("width(pink)", 0, 30, int(shoki/10))
+    g3 = amp3 * np.exp( -((ch - ctr3)/wid3)**2)
+    plt.fill_between(ch, g3, 0, facecolor='pink', alpha=0.3)
+    y = g1 + g2 + g3
 
-    if count > 2:
-        st.sidebar.markdown("---")
-        amp3 = st.sidebar.slider("A(pink)", 0, element.max(), int(element.max()/2))
-        ctr3 = st.sidebar.slider("μ(pink)", 0, ch.max(), int(shoki)+10)
-        wid3 = st.sidebar.slider("width(pink)", 0, 30, int(shoki/10))
-        g3 = amp3 * np.exp( -((ch - ctr3)/wid3)**2)
-        plt.fill_between(ch, g3, 0, facecolor='pink', alpha=0.3)
-        y = g1 + g2 + g3
+if count > 3:
+    st.sidebar.markdown("---")
+    amp4 = st.sidebar.slider("A(red)", 0, element.max(), int(element.max()/2))
+    ctr4 = st.sidebar.slider("μ(red)", 0, ch.max(), int(shoki)+15)
+    wid4 = st.sidebar.slider("width(red)", 0, 30, int(shoki/10))
+    g4 = amp4 * np.exp( -((ch - ctr4)/wid4)**2)
+    plt.fill_between(ch, g4, 0, facecolor='red', alpha=0.3)
+    y = g1 + g2 + g3 + g4
 
-    if count > 3:
-        st.sidebar.markdown("---")
-        amp4 = st.sidebar.slider("A(red)", 0, element.max(), int(element.max()/2))
-        ctr4 = st.sidebar.slider("μ(red)", 0, ch.max(), int(shoki)+15)
-        wid4 = st.sidebar.slider("width(red)", 0, 30, int(shoki/10))
-        g4 = amp4 * np.exp( -((ch - ctr4)/wid4)**2)
-        plt.fill_between(ch, g4, 0, facecolor='red', alpha=0.3)
-        y = g1 + g2 + g3 + g4
-
-    plt.plot(ch, y , ls='-', c='black', lw=1)
-    st.pyplot(plt)
-except:
-    print("aaaaaa")
+plt.plot(ch, y , ls='-', c='black', lw=1)
+st.pyplot(plt)
     
 #フィッティング部分
-try:
-    st.markdown("---")
-    st.subheader("Perform the Fitting")
-    start = st.button("fit!")
+st.markdown("---")
+st.subheader("Perform the Fitting")
+start = st.button("fit!")
 
-    guess_total = []
-    if count == 1:
-        guess_total = [amp1, ctr1, wid1]
-    elif count == 2:
-        guess_total = [amp1, ctr1, wid1, amp2, ctr2, wid2]
-    elif count == 3:
-        guess_total = [amp1, ctr1, wid1, amp2, ctr2, wid2, amp3, ctr3, wid3]
-    elif count == 4:
-        guess_total = [amp1, ctr1, wid1, amp2, ctr2, wid2, amp3, ctr3, wid3, amp4, ctr4, wid4]
+guess_total = []
+if count == 1:
+    guess_total = [amp1, ctr1, wid1]
+elif count == 2:
+    guess_total = [amp1, ctr1, wid1, amp2, ctr2, wid2]
+elif count == 3:
+    guess_total = [amp1, ctr1, wid1, amp2, ctr2, wid2, amp3, ctr3, wid3]
+elif count == 4:
+    guess_total = [amp1, ctr1, wid1, amp2, ctr2, wid2, amp3, ctr3, wid3, amp4, ctr4, wid4]
 
-    bounds = (0, np.inf)
+bounds = (0, np.inf)
 
-    def func(x, *params):
-        num_func = int(len(params)/3)
-        y_list = []
-        for i in range(num_func):
-            y = np.zeros_like(x)
-            param_range = list(range(3*i,3*(i+1),1))
-            amp = params[int(param_range[0])]
-            ctr = params[int(param_range[1])]
-            wid = params[int(param_range[2])]
-            y = y + amp * np.exp( -((x - ctr)/wid)**2)
-            y_list.append(y)
-        y_sum = np.zeros_like(x)
-        for i in y_list:
-            y_sum = y_sum + i
-    #     y_sum = y_sum + params[-1]
-        return y_sum
+def func(x, *params):
+    num_func = int(len(params)/3)
+    y_list = []
+    for i in range(num_func):
+        y = np.zeros_like(x)
+        param_range = list(range(3*i,3*(i+1),1))
+        amp = params[int(param_range[0])]
+        ctr = params[int(param_range[1])]
+        wid = params[int(param_range[2])]
+        y = y + amp * np.exp( -((x - ctr)/wid)**2)
+        y_list.append(y)
+    y_sum = np.zeros_like(x)
+    for i in y_list:
+        y_sum = y_sum + i
+#     y_sum = y_sum + params[-1]
+    return y_sum
 
-    def fit_plot(x, *params):
-        num_func = int(len(params)/3)
-        y_list = []
-        for i in range(num_func):
-            y = np.zeros_like(x)
-            param_range = list(range(3*i,3*(i+1),1))
-            amp = params[int(param_range[0])]
-            ctr = params[int(param_range[1])]
-            wid = params[int(param_range[2])]
-            y = y + amp * np.exp( -((x - ctr)/wid)**2)
-            y_list.append(y)
-        return y_list
+def fit_plot(x, *params):
+    num_func = int(len(params)/3)
+    y_list = []
+    for i in range(num_func):
+        y = np.zeros_like(x)
+        param_range = list(range(3*i,3*(i+1),1))
+        amp = params[int(param_range[0])]
+        ctr = params[int(param_range[1])]
+        wid = params[int(param_range[2])]
+        y = y + amp * np.exp( -((x - ctr)/wid)**2)
+        y_list.append(y)
+    return y_list
 
-    if start:
-        popt, pcov = curve_fit(func, ch, element, p0=guess_total, bounds=bounds)
-except:
-    print("aaaaaaa")
+if start:
+    popt, pcov = curve_fit(func, ch, element, p0=guess_total, bounds=bounds)
     
 #結果の確認
 st.markdown("---")
